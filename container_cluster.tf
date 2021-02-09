250a5b91f5abf6ee7a83a8e3577a82967f8051a7
ortacu <72772410+ortacu@users.noreply.github.com>
2021-02-09 14:36:11 -0600

    Create container_cluster.tf

---
+++ container_cluster.tf
@@ -0,0 +1,20 @@
+resource "google_container_cluster" "primary" {
+  name               = "test-gke-cluster"
+  location           = "us-central1-c"
+  initial_node_count = 2
+  node_config {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = "google_service_account.default.email"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      foo = "bar"
+    }
+    tags = ["foo", "bar"]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}  
